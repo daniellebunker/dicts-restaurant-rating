5dfaf3b587979c84fc2dd370ebcd72c36546e970
Danielle Bunker <daniellebunker8@gmail.com>
2021-03-28 00:27:25 +0000

    added restaurant names to dictionary

--- ratings.py
+++ ratings.py
@@ -1,4 +1,25 @@
 """Restaurant rating lister."""
 
+def restaurant_ratings():
 
-# put your code here
+    restaurant_file = open('scores.txt')
+    restaurant_names = {}
+    #key is restaurasnt name and value is rating
+
+    for line in restaurant_file:
+        line = line.rstrip()
+        line = line.split(":")
+        name = line[0]
+        rating = line[1]
+
+        # print(rating)
+        # print(name)
+
+        if name:
+            restaurant_names[name] = int(rating)
+        
+    print(restaurant_names)
+        
+    return restaurant_names
+
+restaurant_ratings()
